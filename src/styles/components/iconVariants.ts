884486408c7e7471b6eeaca5ba4d1dab4d435bde
Bruno S Velho <bruno.velho@gmail.com>
2025-09-08 22:14:59 +0930

    fix: corrigir erro de digitação na exportação de iconVariants

--- src/styles/components/iconVariants.ts
+++ src/styles/components/iconVariants.ts
@@ -1,6 +1,6 @@
 import { cva } from "class-variance-authority";
 
-export const iconVariantes = cva("fill-gray-400", {
+export const iconVariants = cva("fill-gray-400", {
   variants: {
     animate: {
       false: "",
