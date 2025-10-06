2adf346b5edfe6ab337f47a7a8ccaadbb7ce67ec
Bruno S Velho <bruno.velho@gmail.com>
2025-10-06 21:33:50 +1030

    Prepara Tarefa

--- src/components/core/taskList.tsx
+++ src/components/core/taskList.tsx
@@ -3,18 +3,27 @@ import Button from "../atomic/button";
 import PlusIcon from "../../assets/icons/plus-regular.svg?react"
 import TaskItem from "./taskItem";
 import { useTasks } from "../../hooks/useTasks";
+import { useTask } from "../../hooks/useTask";
 
 export default function TaskList() {
 
   const { tasks } = useTasks();
+  const { prepareTask } = useTask();
 
   console.log(tasks);
 
+  function handleNewTask() {
+    prepareTask()
+  }
 
   return (
     <>
       <section>
-        <Button icon={PlusIcon} className="w-full">Nova Tarefa</Button>
+        <Button
+          icon={PlusIcon}
+          className="w-full"
+          onClick={handleNewTask}
+        >Nova Tarefa</Button>
       </section>
       <section className="space-y-2">
         <TaskItem />
