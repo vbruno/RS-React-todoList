f3c2a771146d0c8f8f80bf21691d00e181dd62f4
Bruno S Velho <bruno.velho@gmail.com>
2025-10-09 22:50:53 +1030

    Atualizando Badges de Resumo de Tarefa

--- src/components/core/taskSummary.tsx
+++ src/components/core/taskSummary.tsx
@@ -1,17 +1,21 @@
+import { useTasks } from "../../hooks/useTasks";
 import Badge from "../atomic/badge";
 import Text from "../atomic/text";
 
 export default function TaskSummary() {
+
+  const { tasksCount, concludedTaskCount } = useTasks()
+
   return (
     <>
       <div className="flex items-center gap-2">
         <Text variant={"body-sm-bold"} className="!text-gray-300" >Tarefas criadas</Text>
-        <Badge variant={"secondary"}>5</Badge>
+        <Badge variant={"secondary"}>{tasksCount}</Badge>
       </div>
 
       <div className="flex items-center gap-2">
         <Text variant={"body-sm-bold"} className="!text-gray-300" >Tarefas concluídas</Text>
-        <Badge variant={"primary"}>2 de 5</Badge>
+        <Badge variant={"primary"}>{concludedTaskCount} de {tasksCount}</Badge>
       </div>
     </>
   )
--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,12 +1,12 @@
 import useLocalStorage from "use-local-storage";
-import { TASKS_KEY, type Task } from "../models/task";
+import { TASKS_KEY, TaskState, type Task } from "../models/task";
 
 export function useTasks() {
   const [tasks] = useLocalStorage<Task[]>(TASKS_KEY, [])
 
   return {
     tasks,
-    tasksCount: tasks.length,
+    tasksCount: tasks.filter((task) => task.state === TaskState.Created).length,
     concludedTaskCount: tasks.filter((task) => task.concluded ).length
   }
 }
